b780505e01dead7f4c17089dd0fe675ac52637ba
Keshav Shah <114270838+Keshav-Shah@users.noreply.github.com>
2023-06-26 17:11:08 -0500

    Update FloorInBST.py

--- Assignment-2/FloorInBST.py
+++ Assignment-2/FloorInBST.py
@@ -20,39 +20,57 @@ Time Spent: 38 minutes
 '''
 
 class Node:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
+    def __init__(self, data=0, left=None, right=None):
+        self.key = data
         self.left = left
         self.right = right
 
-def floor_in_bst(node, target):
-    if not node:
+def floor_in_bst(root, input):
+    # Base cases
+    if root == None:
         return -1
+    if root.key == input:
+        return input
 
-    if not node.left and not node.right:
-        return node.val
+    # If root's value is smaller, try in
+    # right subtree
+    elif root.key < input:
+        k = floor_in_bst(root.right, input)
+        if k == -1:
+            return root.key
+        else:
+            return k
 
-    if node.val == target:
-        return node.val
-
-    # traverse left subtree
-    elif node.val > target:
-        return floor_in_bst(node.left, target)
-
-    # traverse right subtree
-    elif node.val < target:
-        return floor_in_bst(node.right, target)
+    # If root's key is greater, return
+    # value from left subtree.
+    elif root.key > input:
+        return floor_in_bst(root.left, input)
 
 def main():
     node20 = Node(20, None, None)
     node17 = Node(17, None, node20)
     node13 = Node(13, None, None)
     node16 = Node(16, node13, node17)
-    node9 = (9, None, None)
-    node8 = (8, None, node9)
+    node9 = Node(9, None, None)
+    node8 = Node(8, None, node9)
     root = Node(10, node8, node16)
 
     print(floor_in_bst(root, 13)) # Floor : 13
     print(floor_in_bst(root, 15)) # Floor : 13
+
+    # Convoluted BST test case
+    node13 = Node(13, None, None)
+    node16 = Node(16, None, None)
+    node14 = Node(14, node13, None)
+    node15 = Node(15, node14, node16)
+    node20 = Node(20, node15, None)
+    node0 = Node(2, None, None)
+    root = Node(10, node0, node20)
+
+    print(floor_in_bst(root, 13)) # Floor : 13
+    print(floor_in_bst(root, 19)) # Floor : 16
+    print(floor_in_bst(root, 17)) # Floor : 16
+    print(floor_in_bst(root, 9)) # Floor : 2
+    print(floor_in_bst(root, 1)) # Floor : Not existent so -1 returned
 
 main()
